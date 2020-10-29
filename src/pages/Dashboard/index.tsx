4066600a701b65a945d14afa01deeb4b8013f661
Luiz Honorato <lhehonorato@gmail.com>
2020-10-29 20:54:30 -0300

    dashboard header.

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,7 +1,64 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
+import { Link } from 'react-router-dom';
+import {
+  Container,
+  Header,
+  HeaderContent,
+  AvatarArea,
+  Dropdown,
+  DropdownContent,
+} from './styles';
+import { User } from '../../store/modules/auth/types';
+import { IState } from '../../store';
+
+import logoImg from '../../assets/logo.svg';
+import avatarImg from '../../assets/avatar-wow-flix.png';
 
 const Dashboard: React.FC = () => {
-  return <h1>Dashboard</h1>;
+  const user = useSelector<IState, User | null>(state => state.auth.user);
+
+  return (
+    <Container>
+      <Header>
+        <HeaderContent>
+          <img src={logoImg} alt="Wowflix Logo" />
+          <ul>
+            <li>
+              <Link to="/dashboard">Filmes</Link>
+            </li>
+            <li>
+              <Link to="/my-list">Minha lista</Link>
+            </li>
+          </ul>
+          <AvatarArea>
+            <Dropdown>
+              <img src={avatarImg} alt="Avatar" />
+              <DropdownContent>
+                <ul>
+                  <li>
+                    <div>
+                      <span>Bem-vindo,</span>
+                      <strong>{user?.name}</strong>
+                    </div>
+                  </li>
+                  <li>
+                    <Link to="/profile">Minha conta</Link>
+                  </li>
+                  <li>
+                    <Link to="/dashboard">Planos</Link>
+                  </li>
+                  <li>
+                    <Link to="/my-list">Sair</Link>
+                  </li>
+                </ul>
+              </DropdownContent>
+            </Dropdown>
+          </AvatarArea>
+        </HeaderContent>
+      </Header>
+    </Container>
+  );
 };
 
 export default Dashboard;
